23a8c9ac4aa2f0e0f61cee59c7aaef4f61843676
Baoanh2610 <baoanhdeptrai2610@gmail.com>
2025-03-28 02:00:09 +0700

    nhieu nguoi dung

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
     plugins: [react()],
@@ -8,5 +9,15 @@ export default defineConfig({
         outDir: "dist",
         sourcemap: true,
         minify: "terser",
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, "index.html"),
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
 });
